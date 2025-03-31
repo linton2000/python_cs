9c17b4ca1ea984d153b48d4dbb3add46768af75d
Linton Charles <lintoncharles2000@gmail.com>
2025-04-01 09:53:01 +1100

    Add abstract class for Queue ADT

---
+++ data_structures/queue.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod
+from typing import TypeVar
+
+T = TypeVar('T')
+
+
+class Queue:
+    """ Abstract class for various Queue ADT implementations.
+    """
+
+    def __init__(self) -> None:
+        self.size = 0
+    
+    @abstractmethod
+    def enqueue(self, element) -> None:
+        """ Append element to end of queue.
+        """
+        pass
+    
+    @abstractmethod
+    def dequeue(self) -> T:
+        """ Remove element from start of queue (FIFO order).
+        """
+        pass
+    
+    @abstractmethod
+    def peek(self) -> T:
+        """ Read element at front of queue without modifications.
+        """
+        pass
