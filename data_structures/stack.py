700ee9e859249bd581e0de04f7b5f8f911196118
Linton Charles <lintoncharles2000@gmail.com>
2025-04-01 09:25:37 +1100

    Add ArrayStack test case and fix implementation

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -34,11 +34,11 @@ class ArrayStack(Stack):
     """
 
     def __init__(self) -> None:
-        Stack.__init__()
+        Stack.__init__(self)
         self.array = [None]  # Will be superceded by first pushed element.
     
     def push(self, element) -> None:
-        self.array[self.size] = element
+        self.array.append(element)
         self.size += 1
     
     def pop(self) -> T:
@@ -46,4 +46,17 @@ class ArrayStack(Stack):
 
     def peek(self) -> T:
         return self.array[-1]
+
+
+if __name__ == '__main__':
+    
+    # Test string reversal using ArrayStack
+    inStr = '!selrahC notniL si eman ym ,olleH'
+    stack = ArrayStack()
+    for c in inStr:
+        stack.push(c)
+    
+    for _ in inStr:
+        print(stack.pop(), end='') # The `end` param ensures same line print.
+    print()
     
