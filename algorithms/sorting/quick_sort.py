89ffb4611f5f98aa25e506fbd4c379ba2059ef44
Linton Charles <lintoncharles2000@gmail.com>
2025-03-31 11:03:51 +1100

    Implement naive quicksort

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -1,14 +1,41 @@
 # Recursively pick a pivot (as close to median as possible) and move all smaller elements to its left
 # and all larger elements to its right. 
+from tester import Tester
 
-def quicksort(arr: list, lo: int = None, hi: int = None) -> list:
-    if not lo and not hi:
-        lo = 0
-        hi = len(arr) - 1
 
-    while lo < hi:
-        pivot = _median_of_three(arr, lo, hi)
-        
+def quick_sort(arr: list) -> list:
+    # Recursion Base Case
+    if len(arr) <= 1:
+        return arr
+
+    # Ensures all elements left of pvt are smaller, and all elements to the right are greater than pvt.
+    less, equal, greater = partition_naive(arr)
+    
+    left = quick_sort(less)
+    right = quick_sort(greater)
+    return left + equal + right
+
+def partition_naive(arr: list) -> list:
+    """ 
+    1. Iterates through full input arr.
+        a. Appends all elements smaller than pvt to `less` array.
+        b. Appends all elements larger than pvt to `greater` array.
+        c. Appends all elements equal to pvt to `equal` array (to handle duplicates).
+    """
+    pvt_i = _median_of_three(arr, 0, len(arr) - 1)
+
+    i = 0
+    less, greater, equal = [], [], []
+    while i < len(arr):
+        if arr[i] < arr[pvt_i]:
+            less.append(arr[i])
+        elif arr[i] > arr[pvt_i]:
+            greater.append(arr[i])
+        else:
+            equal.append(arr[i])
+        i += 1
+    
+    return less, equal, greater
 
 def _median_of_three(arr: list, lo: int, hi: int) -> int:
     """ Sort 1st, mid and last element of arr. Mid element is median of three and 
@@ -22,3 +49,7 @@ def _median_of_three(arr: list, lo: int, hi: int) -> int:
     if arr[lo] > arr[mid]:
         arr[lo], arr[mid] = arr[mid], arr[lo]
     return mid
+
+if __name__ == '__main__':
+    tester = Tester()
+    tester.test(quick_sort, "Quick Sort")
