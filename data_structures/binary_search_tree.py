5a3716dc5f4f06796261b2f6fc489f0061599abc
Linton Charles <lintoncharles2000@gmail.com>
2025-04-05 09:41:37 +1100

    Implement the insert method in the LinkedListBST definition

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -1,8 +1,6 @@
 from abc import abstractmethod
-from typing import TypeVar
-from linked_list import ListNode
-
-T = TypeVar('T')
+from typing import Optional
+from node import TreeNode
 
 
 class BinarySearchTree:
@@ -10,17 +8,83 @@ class BinarySearchTree:
     """
 
     def __init__(self):
+        self.size = 0
+
+    @abstractmethod
+    def search(self, target_val) -> TreeNode:
+        """ Traverse the BST to find and return the target_val node. Returns False
+        if target_val node doesn't exist in BST.
+        """
         pass
 
     @abstractmethod
-    def search(self, target):
+    def insert(self, new_val) -> None:
+        """ Sinks the newly inserted node into the right position in the BST.
+        Keeps the BST property but does not try to keep a balanced BST.
+        """
         pass
 
     @abstractmethod
-    def insert(self, target):
+    def remove(self, target_val) -> None:
+        """ Finds and removes the target_val node from the BST.
+        """
         pass
 
     @abstractmethod
-    def remove(self, target):
+    def get_min(self, root: TreeNode = None) -> TreeNode:
+        """ Finds and returns the node with the minimum value.
+        """
         pass
+
+    @abstractmethod
+    def get_max(self, root: TreeNode = None) -> TreeNode:
+        """ Finds and returns the node with the maximum value.
+        """
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        """ Returns a string representation of the BST.
+        """
+        pass
+
+
+class LinkedListBST(BinarySearchTree):
+    """ A linked list-based implementation of a Binary Search Tree.
+    Assumes there are no duplicate node values.
+    """
     
+    def __init__(self):
+        super().__init__()
+        self.root: Optional[TreeNode] = None
+    
+    def search(self, target_val) -> TreeNode:
+        pass
+
+    def insert(self, new_val) -> None:
+        def _recursive_insert(root: TreeNode, val) -> TreeNode:
+            if not root:  # Return new node to previous frame in call stack
+                return TreeNode(val)
+            if val > root.val: # Go down right subtree
+                root.right = _recursive_insert(root.right, val)
+            elif val < root.val: # Go down left subtree
+                root.left = _recursive_insert(root.left, val)
+
+        if self.root == None:
+            self.root = TreeNode(new_val)
+        else:
+            _recursive_insert(self.root, new_val)
+        self.size += 1
+            
+    def remove(self, target_val) -> None:
+        pass
+
+    def __str__(self) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    bst = LinkedListBST()
+    bst.insert(1)
+    bst.insert(4)
+    bst.insert(8)
