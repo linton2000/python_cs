80eae451c2674f8541d3ce9c2d85f781093b3506
Linton Charles <lintoncharles2000@gmail.com>
2025-04-04 14:10:14 +1100

    Fix error in Queue implementation

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -8,22 +8,21 @@ T = TypeVar('T')
 class Queue:
     """ Abstract class for various Queue ADT implementations.
     """
-
     def __init__(self) -> None:
         self.size = 0
-    
+
     @abstractmethod
     def enqueue(self, element) -> None:
         """ Append element to end of queue.
         """
         pass
-    
+
     @abstractmethod
     def dequeue(self) -> T:
         """ Remove element from start of queue (FIFO order).
         """
         pass
-    
+
     @abstractmethod
     def peek(self) -> T:
         """ Read element at front of queue without modifications.
@@ -31,42 +30,54 @@ class Queue:
         pass
 
 
-class LinkedListQueue:
+class LinkedListQueue(Queue):
     """ A linked list-based implementation of the Queue ADT.
     """
-
     def __init__(self) -> None:
-        self.size = 0
-        self.root = None
-    
+        Queue.__init__(self)
+        self.head = None  # front of queue
+        self.tail = None  # back of queue
+
     def enqueue(self, element) -> None:
-        if self.root:
-            self.root.next = ListNode(element)
+        new_node = ListNode(element)
+        
+        if self.tail:
+            self.tail.next = new_node  # Add to the end
+            self.tail = new_node
         else:
-            self.root = ListNode(element)
+            # Queue is empty
+            self.head = new_node
+            self.tail = new_node
+            
         self.size += 1
-    
+
     def dequeue(self) -> T:
-        if not self.root:
-            self.root = None
-        tmp = self.root
-        self.root = self.root.next
+        if not self.head:
+            return None
+            
+        tmp = self.head
+        self.head = self.head.next
+        
+        # If we removed the last element, update tail
+        if not self.head:
+            self.tail = None
+            
         self.size -= 1
-        return tmp
-    
+        return tmp.val  # Return data, not the node
+
     def peek(self) -> T:
-        return self.root
+        if not self.head:
+            return None
+        return self.head.val  # Return data, not the node
 
 
 if __name__ == '__main__':
-
     def test_str_order(queue: Queue):
         inStr = 'Hello, my name is Linton Charles!'
         for c in inStr:
             queue.enqueue(c)
-        
         for _ in range(queue.size):
             print(queue.dequeue(), end='')
         print()
-    
+        
     test_str_order(LinkedListQueue())
