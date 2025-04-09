1ed202707e88f2ed153a2212c097a85bddf80ce4
Linton Charles <lintoncharles2000@gmail.com>
2025-04-09 13:23:12 +1000

    Rename TreeNode __repr__ method to get_details

--- data_structures/node.py
+++ data_structures/node.py
@@ -23,11 +23,11 @@ class TreeNode:
         self.left: Optional[TreeNode] = None
         self.right: Optional[TreeNode] = None
 
-    def __str__(self) -> str:
-        return str(self.val)
-    
-    def __repr__(self) -> str:
+    def get_details(self) -> str:
         left = self.left.val if self.left else None
         right = self.right.val if self.right else None
         return f'TreeNode: {str(self.val)}\n- Left Child: {str(left)}\n- Right Child: {str(right)}'
+
+    def __str__(self) -> str:
+        return str(self.val)
     
