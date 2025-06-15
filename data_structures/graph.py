21edbe74a1a9823c3c1f3bbe602c98ed805c9672
Linton Charles <lintoncharles2000@gmail.com>
2025-06-15 10:28:39 +1000

    Implement graph plotting

--- data_structures/graph.py
+++ data_structures/graph.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 import matplotlib.pyplot as plt
 import networkx as nx
 
+
 class Graph:
     """ Abstract class for computational graphs.
     """
@@ -14,6 +15,17 @@ class Graph:
     @abstractmethod
     def plot_graph(self):
         pass
+
+    def _draw_netx_graph(self):
+        # Draw the graph
+        pos = nx.spring_layout(self.net_graph)
+        nx.draw(self.net_graph, pos, with_labels=True, node_color='skyblue', node_size=1000, arrows=True)
+
+        # Draw edge labels (weights)
+        edge_labels = nx.get_edge_attributes(self.net_graph, 'weight')
+        nx.draw_networkx_edge_labels(self.net_graph, pos, edge_labels=edge_labels)
+        
+        plt.show()
 
 
 class GridMatrix(Graph):
@@ -33,15 +45,12 @@ class AdjacencyMatrix(Graph):
             # Iterate through matrix & add all edges
             for i in range(len(self.graph)):
                 for j in range(len(self.graph)):
-                    if self.graph[i][j] == 1:
-                        self.net_graph.add_edge(i, j)
-                    if self.graph[j][i] == 1:
-                        self.net_graph.add_edge(j, i)
-        
-        # Draw the graph
-        pos = nx.spring_layout(self.net_graph)
-        nx.draw(self.net_graph, pos, with_labels=True, node_color='skyblue', node_size=1000, arrows=True)
-        plt.show()
+                    if self.graph[i][j] != 0:
+                        self.net_graph.add_edge(i, j, weight=self.graph[i][j])
+                    if self.graph[j][i] != 0:
+                        self.net_graph.add_edge(j, i, weight=self.graph[j][i])
+
+        self._draw_netx_graph()
 
 
 class AdjacencyList(Graph):
@@ -57,9 +66,9 @@ if __name__ == '__main__':
     #
     #
     adjacency_matrix = [
-        [0, 1, 1],
-        [1, 0, 0],
-        [1, 0, 0]
+        [0, 0, 0],
+        [2, 0, 0],
+        [2, 0, 0]
     ]
     graph = AdjacencyMatrix(adjacency_matrix)
     graph.plot_graph()
